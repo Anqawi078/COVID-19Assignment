bd815f56795213db56cbaa3c9aeaa605fd797a3c
khalil <khalil.anqawi@erabia.com>
2022-01-21 00:31:01 +0200

    Initial Commit

---
+++ COVID-19Assignment/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  COVID-19Assignment
+//
+//  Created by khalil on 21/1/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
